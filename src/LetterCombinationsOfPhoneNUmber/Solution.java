00891f14251bbb99c72c1dd35b37d9c3964d2ce2
wihoho <wihoho@gmail.com>
2014-03-02 00:13:33 +0800

    Solve four more questions

---
+++ src/LetterCombinationsOfPhoneNUmber/Solution.java
@@ -0,0 +1,20 @@
+package LetterCombinationsOfPhoneNUmber;
+
+import java.util.ArrayList;
+
+/**
+ * Authored by GongLi at 23:55 on 1/3/14
+ */
+
+
+public class Solution {
+
+    public ArrayList<String> letterCombinations(String digits) {
+
+
+        String[] combinations = {"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ArrayList<String> result = new ArrayList<String>();
+
+        String[] resultDigits = new String[digits.length()];
+    }
+}
--- src/ThreeSum/Solution.java
+++ src/ThreeSum/Solution.java
@@ -11,27 +11,44 @@ import java.util.Arrays;
 public class Solution {
 
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 
-        if(num == null || num.length <= 2) return null;
+        if(num == null || num.length <= 2) return result;
 
         Arrays.sort(num);
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         int size = num.length;
+
         for(int i = 0; i <= size - 3; i ++ ){
+            if(i > 0 && num[i] == num[i-1])
+                continue;
+
             int low = i + 1;
             int high = size - 1;
 
             while (low < high){
-                if(num[i] + num[low] + num[high] == 0){
+                int sum = num[i] + num[low] + num[high];
+                if(sum == 0){
                     ArrayList<Integer> cur = new ArrayList<Integer>();
                     cur.add(num[i]);
                     cur.add(num[low]);
                     cur.add(num[high]);
 
                     result.add(cur);
+
+
+
+                    low ++;
+                    while(low < size-2 && num[low - 1] == num[low])
+                        low ++;
+
+                    high --;
+                    while(high > i && num[high + 1] == num[high])
+                        high --;
+
+
                 }
 
-                else if(num[i] + num[low] + num[high] > 0)
+                else if(sum > 0)
                     high --;
 
                 else
@@ -39,6 +56,14 @@ public class Solution {
             }
         }
         return result;
+    }
 
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] arr = {-2,0,1,1,2};
+        s.threeSum(arr);
+
+        System.out.println();
     }
 }
