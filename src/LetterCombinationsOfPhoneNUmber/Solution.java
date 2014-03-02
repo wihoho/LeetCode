e0512c6af9dd231d0cb19c70d811aec054a9ac56
wihoho <wihoho@gmail.com>
2014-03-02 15:17:23 +0800

    Solve 6 more questions

--- src/LetterCombinationsOfPhoneNUmber/Solution.java
+++ src/LetterCombinationsOfPhoneNUmber/Solution.java
@@ -16,5 +16,33 @@ public class Solution {
         ArrayList<String> result = new ArrayList<String>();
 
         String[] resultDigits = new String[digits.length()];
+        for(int i = 0; i < resultDigits.length; i ++)
+            resultDigits[i] = combinations[digits.charAt(i) - '0'];
+
+        StringBuilder sb = new StringBuilder();
+        getCombination(resultDigits, result, 0, sb);
+
+        return result;
+    }
+
+    public void getCombination(String[] all, ArrayList<String> container, int step, StringBuilder re){
+
+        int size = all.length;
+        if(step == size){
+            container.add(re.toString());
+            return;
+        }
+
+        String cur = all[step];
+        for(int i = 0; i < cur.length(); i ++){
+            re.append(cur.charAt(i));
+            getCombination(all, container, step + 1, re);
+            re.deleteCharAt(re.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.letterCombinations("23"));
     }
 }
